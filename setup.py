a15f76ee4afce60075587023ef19dcd3ce43c5ca
YuzuRyo61 <yuzuryo61@yuzulia.com>
2018-11-24 19:14:33 +0900

    added long_description and classifiers to setup.py ✌

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ import sys
 
 sys.path.append('./test')
 
+with open('README.md', encoding="utf-8") as df:
+	ld = df.read()
+
 setup(
     name='Misskey.py',
     version='0.3.1',
     description='The Misskey library for Python. Misskey is made by syuilo.',
+	long_description=ld,
+	keywords="Misskey, API, syuilo",
     author='YuzuRyo61',
     url='https://github.com/YuzuRyo61/Misskey.py',
     license='MIT',
@@ -16,5 +21,17 @@ setup(
         'websocket-client'
     ],
     packages=find_packages(exclude=('sample', 'docs', 'tests')),
-    test_suite = 'MISTEST.UNITTEST_FUNCTION'
+    test_suite = 'MISTEST.UNITTEST_FUNCTION',
+	classifiers=[
+		'License :: OSI Approved :: MIT License',
+		'Natural Language :: Japanese',
+		'Programming Language :: Python :: 3 :: Only',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.7',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Software Development :: Libraries',
+		'Development Status :: 3 - Alpha',
+		'Topic :: Internet :: WWW/HTTP',
+		'Topic :: Internet'
+	]
 )
