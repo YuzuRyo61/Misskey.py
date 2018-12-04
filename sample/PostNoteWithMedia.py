f395456b8bfa756d2e5da8d2dde0a90c887c4f5f
YuzuRyo61 <yuzuryo61@yuzulia.com>
2018-12-04 21:57:32 +0900

    sample script updated 🔝

--- sample/PostNoteWithMedia.py
+++ sample/PostNoteWithMedia.py
@@ -5,6 +5,7 @@ except ImportError:
     sys.path.append("../")
     from Misskey import Misskey
     from Misskey.Exceptions import *
+import os, tkinter, tkinter.filedialog, sys
 
 if __name__ == "__main__":
     with open("key/application.txt", "r") as appkey:
@@ -19,14 +20,35 @@ if __name__ == "__main__":
 
     misskey = Misskey(instance, appSecret=appSecret, accessToken=userToken)
 
-    body = input("Post to Misskey: ")
+    root = tkinter.Tk()
+    root.withdraw()
+    fileType = [("", "*")]
+    initDir = os.path.abspath(os.path.dirname(__file__))
+
+    print("Please select your file.")
+    file = tkinter.filedialog.askopenfilename(filetypes = fileType, initialdir = initDir)
+
+    if file == '' or file == None:
+        print("You didn't select a file. exiting.")
+        input("[ENTER TO EXIT]")
+        sys.exit()
+
+    print("You will be upload it: {}".format(file))
+
+    body = input("Post to Misskey (Can be leave a blank): ")
+
+    if body == '':
+        body = None
 
     try:
-        res = misskey.notes_post(body)
+        fileupload = misskey.drive_files_create(file)
+        res = misskey.notes_create(body, fileIds=[fileupload['id']])
     except MisskeyBadRequestException:
         print("Failed!")
     except MisskeyResponseException:
         print("Failed!")
+    except:
+        print("Failed other exception!")
     else:
         print("Success!")
     finally:
