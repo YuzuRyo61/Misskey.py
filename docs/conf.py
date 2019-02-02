b8b355ea1b2b34dc2011a8ef16d2ef05df785f7d
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-02-02 12:45:27 +0900

    Modified document configuration ✎

--- docs/conf.py
+++ docs/conf.py
@@ -21,10 +21,6 @@ source_parsers = {
     '.md': CommonMarkParser,
 }
 
-source_suffix = ['.rst', '.md']
-html_theme = 'sphinx_rtd_theme'
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'Misskey.py'
@@ -59,8 +55,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
@@ -86,7 +82,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
