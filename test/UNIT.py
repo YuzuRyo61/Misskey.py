0a7b0a32c6eb8224c31627fae3d70c6554c5f294
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-09-02 15:28:31 +0900

    Unit test suite updated

--- test/UNIT.py
+++ test/UNIT.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import unittest
 import configparser
 import sys
@@ -18,302 +17,264 @@ class MisskeyPyUnitTest(unittest.TestCase):
         cls.unit = Misskey(config['instance']['address'])
         cls.unitI = Misskey(config['instance']['address'], i=config['key']['i'])
 
-    def test_exceptions(self):
-        print("UNIT\t=> [EXCEPTIONS]")
-        
-        print("\t\t=> Invalid Address [MisskeyInitException]")
+    def test_exception_init(self):
         self.assertRaises(MisskeyInitException, lambda: Misskey('yuzulia.com'))
         
-        print("\t\t=> Invalid APIToken(i) [MisskeyAPITokenException]")
+    def test_exception_apitoken(self):
         self.assertRaises(MisskeyAPITokenException, lambda: Misskey(config['instance']['address'], i="invalid"))
-        
-        print("\t\t=> No APIToken(i) [MisskeyAiException]")
+    
+    def test_exception_ai(self):
         self.assertRaises(MisskeyAiException, lambda: self.unit.i())
         
-        print("\t\t=> API Error [MisskeyAPIException]")
+    def test_exception_api(self):
         self.assertRaises(MisskeyAPIException, lambda: self.unitI.notes_create())
 
-        print("\t\t=> Specified file is not found [MisskeyFileException]")
+    def test_exception_file(self):
         self.assertRaises(MisskeyFileException, lambda: self.unitI.drive_files_create("hoge"))
         
-        print("DONE\t=> [EXCEPTIONS]\n")
-
-    def test_Util(self):
-        print("UNIT\t=> [UTILITY]")
-        
-        print("\t\t=> hash_apitoken")
+    def test_Util_hashapitoken(self):
         hashed = MisskeyUtil.hash_apitoken(config['key']['accesstoken'], config['key']['appsecret'])
         self.assertEqual(config['key']['i_app_access'], hashed)
         
-        print("\t\t=> hash_apitoken => Should usable")
         mk = Misskey(config['instance']['address'], i=hashed)
         mkI = mk.i()
         self.assertEqual(type(mkI), dict)
         
-        print("\t\t=> username_available")
+    def test_Util_usernameavaliable(self):
         self.assertEqual(type(MisskeyUtil.username_available(config['instance']['address'], "hoge")), dict)
 
-        print("DONE\t=> [UTILITY]\n")
-
-    def test_other(self):
-        print("UNIT\t=> [OTHER]")
-        
-        print("\t\t=> delete apiToken")
+    def test_deletable_apiToken(self):
         uniti_other = Misskey(config['instance']['address'], i=config['key']['i'])
         del uniti_other.apiToken
         self.assertEqual(uniti_other.apiToken, None)
 
-        print("\t\t=> read address")
-        self.assertEqual(uniti_other.address, config['instance']['address'])
-
-        print("DONE\t=> [OTHER]")
-
+    def test_read_address(self):
+        self.assertEqual(self.unit.address, config['instance']['address'])
 
     def test_meta(self):
-        print("UNIT\t=> meta")
         res = self.unit.meta()
         self.assertEqual(type(res), dict)
-        print("DONE\t=> meta\n")
 
     def test_stats(self):
-        print("UNIT\t=> stats")
         res = self.unit.stats()
         self.assertEqual(type(res), dict)
-        print("DONE\t=> stats\n")
 
     def test_i(self):
-        print("UNIT\t=> i")
-
-        print("\t\t=> i")
         self.assertEqual(type(self.unitI.i()), dict)
 
-        print("\t\t=> i_favorites")
+    def test_i_favorites(self):
         self.assertEqual(type(self.unitI.i_favorites()), list)
 
-        print("\t\t=> i_notifications")
+    def test_i_notifications(self):
         self.assertEqual(type(self.unitI.i_notifications()), list)
 
-        print("\t\t=> i_readAllMessagingMessages")
+    def test_i_readAllMessagingMessages(self):
         self.assertTrue(self.unitI.i_readAllMessagingMessages())
 
-        print("\t\t=> i_readAllUnreadNotes")
+    def test_i_readAllUnreadNotes(self):
         self.assertTrue(self.unitI.i_readAllUnreadNotes())
 
-        print("\t\t=> notifications_markAllAsRead")
+    def test_notifications_markAllAsRead(self):
         self.assertTrue(self.unitI.notifications_markAllAsRead())
 
-        print("\t\t=> i_update")
+    def test_i_update(self):
         self.assertEqual(type(self.unitI.i_update()), dict)
 
-        print("DONE\t=> i\n")
-
-    def test_notes(self):
-        print("UNIT\t=> notes")
-        
-        print("\t\t=> notes_create")
+    def test_0_notes_create(self):
         res_ncv = self.unitI.notes_create("Misskey.py notes testing...")
         res_vote = self.unitI.notes_create("Misskey.py vote notes testing...", poll=['choice1', 'choice2'])
         self.assertEqual(type(res_ncv), dict)
+        self.__class__.noteId = res_ncv['createdNote']['id']
+        self.__class__.noteId_vote = res_vote['createdNote']['id']
+    
+    def test_1_notes_show(self):
+        self.assertEqual(type(self.unitI.notes_show(self.__class__.noteId)), dict)
+
+    def test_1_notes_renotes(self):
+        self.assertEqual(type(self.unitI.notes_renotes(self.__class__.noteId)), list)
+
+    def test_1_notes_polls_vote(self):
+        self.assertTrue(self.unitI.notes_polls_vote(self.__class__.noteId_vote, 0))
+
+    def test_1_i_pin(self):
+        self.assertEqual(type(self.unitI.i_pin(self.__class__.noteId_vote)), dict)
+
+    def test_8_i_unpin(self):
+        self.assertEqual(type(self.unitI.i_unpin(self.__class__.noteId_vote)), dict)
+
+
+    def test_9_notes_delete(self):
+        self.assertTrue(self.unitI.notes_delete(self.__class__.noteId))
+        self.assertTrue(self.unitI.notes_delete(self.__class__.noteId_vote))
         
-        print("\t\t=> notes_show")
-        self.assertEqual(type(self.unitI.notes_show(res_ncv['createdNote']['id'])), dict)
-
-        print("\t\t=> notes_renotes")
-        self.assertEqual(type(self.unitI.notes_renotes(res_ncv['createdNote']['id'])), list)
-
-        print("\t\t=> notes_polls_vote")
-        self.assertTrue(self.unitI.notes_polls_vote(res_vote['createdNote']['id'], 0))
-
-        print("\t\t=> i_pin")
-        self.assertEqual(type(self.unitI.i_pin(res_ncv['createdNote']['id'])), dict)
-
-        print("\t\t=> i_unpin")
-        self.assertEqual(type(self.unitI.i_unpin(res_ncv['createdNote']['id'])), dict)
-
-        print("\t\t=> notes_delete")
-        self.assertTrue(self.unitI.notes_delete(res_ncv['createdNote']['id']))
-        self.assertTrue(self.unitI.notes_delete(res_vote['createdNote']['id']))
-        
-        print("\t\t=> notes_reactions_create")
+    def test_0_notes_reactions_create(self):
         self.assertTrue(self.unitI.notes_reactions_create(config['note']['targetReaction'], 0))
 
-        print("\t\t=> notes_reactions")
+    def test_1_notes_reactions(self):
         self.assertEqual(type(self.unitI.notes_reactions(config['note']['targetReaction'])), list)
 
-        print("\t\t=> notes_reactions_delete")
+    def test_9_notes_reactions_delete(self):
         self.assertTrue(self.unitI.notes_reactions_delete(config['note']['targetReaction']))
 
-        print("\t\t=> notes_favorites_create")
+    def test_0_notes_favorites_create(self):
         self.assertTrue(self.unitI.notes_favorites_create(config['note']['targetReaction']))
 
-        print("\t\t=> notes_favorites_delete")
+    def test_9_notes_favorites_delete(self):
         self.assertTrue(self.unitI.notes_favorites_delete(config['note']['targetReaction']))
 
-        print("\t\t=> notes")
+    def test_notes(self):
         self.assertEqual(type(self.unitI.notes()), list)
 
-        print("\t\t=> notes_globalTimeline")
+    def test_globalTimeline(self):
         self.assertEqual(type(self.unitI.notes_globalTimeline()), list)
 
-        print("\t\t=> notes_hybridTimeline")
+    def test_hybridTimeline(self):
         self.assertEqual(type(self.unitI.notes_hybridTimeline()), list)
 
-        print("\t\t=> notes_localTimeline")
+    def test_localTimeline(self):
         self.assertEqual(type(self.unitI.notes_localTimeline()), list)
 
-        print("\t\t=> notes_userListTimeline")
+    def test_userListTimeline(self):
         self.assertEqual(type(self.unitI.notes_userListTimeline(config['lists']['target'])), list)
-
-        print("DONE\t=> notes\n")
     
     def test_users(self):
-        print("UNIT\t=> users")
-
-        print("\t\t=> users")
         self.assertEqual(type(self.unit.users()), list)
 
-        print("\t\t=> users_show")
+    def test_users_show(self):
         self.assertEqual(type(self.unit.users_show(userId=config['user']['target'])), dict)
 
-        print("\t\t=> users_show (many)")
+    def test_users_showMulti(self):
         self.assertEqual(type(self.unit.users_show(userIds=[config['user']['target']])), list)
 
-        print("\t\t=> users_notes")
+    def test_users_notes(self):
         self.assertEqual(type(self.unitI.users_notes(config['user']['target'])), list)
 
-        print("\t\t=> users_lists_create")
+    def test_0_users_lists_create(self):
         ulc = self.unitI.users_lists_create("test")
         self.assertEqual(type(ulc), dict)
+        self.__class__.listId = ulc['id']
 
-        print("\t\t=> users_lists_push")
-        self.assertTrue(self.unitI.users_lists_push(ulc['id'], config['user']['target']))
+    def test_1_users_lists_push(self):
+        self.assertTrue(self.unitI.users_lists_push(self.__class__.listId, config['user']['target']))
 
-        print("\t\t=> users_lists_show")
-        self.assertEqual(type(self.unitI.users_lists_show(ulc['id'])), dict)
+    def test_1_users_lists_show(self):
+        self.assertEqual(type(self.unitI.users_lists_show(self.__class__.listId)), dict)
 
-        print("\t\t=> users_lists_pull")
-        self.assertTrue(self.unitI.users_lists_pull(ulc['id'], config['user']['target']))
+    def test_8_users_lists_pull(self):
+        self.assertTrue(self.unitI.users_lists_pull(self.__class__.listId, config['user']['target']))
 
-        print("\t\t=> users_lists_update")
-        self.assertEqual(type(self.unitI.users_lists_update(ulc['id'], "test_rename")), dict)
+    def test_8_users_lists_update(self):
+        self.assertEqual(type(self.unitI.users_lists_update(self.__class__.listId, "test_rename")), dict)
 
-        print("\t\t=> users_lists_delete")
-        self.assertTrue(self.unitI.users_lists_delete(ulc['id']))
+    def test_9_users_lists_delete(self):
+        self.assertTrue(self.unitI.users_lists_delete(self.__class__.listId))
 
-        print("\t\t=> users_lists_list")
+    def test_users_lists_list(self):
         self.assertEqual(type(self.unitI.users_lists_list()), list)
 
-        print("\t\t=> users_followers")
+    def test_users_followers(self):
         self.assertEqual(type(self.unit.users_followers(userId=config['user']['target'])), list)
 
-        print("\t\t=> users_following")
+    def test_users_following(self):
         self.assertEqual(type(self.unit.users_following(userId=config['user']['target'])), list)
 
-        print("\t\t=> following_create")
+    def test_0_following_create(self):
         self.assertEqual(type(self.unitI.following_create(config['user']['target'])), dict)
 
-        print("\t\t=> following_delete")
+    def test_0_following_delete(self):
         self.assertEqual(type(self.unitI.following_delete(config['user']['target'])), dict)
 
-        print("\t\t=> mute_create")
+    def test_0_mute_create(self):
         self.assertTrue(self.unitI.mute_create(config['user']['target']))
 
-        print("\t\t=> mute_list")
+    def test_mute_list(self):
         self.assertEqual(type(self.unitI.mute_list()), list)
 
-        print("\t\t=> mute_delete")
+    def test_9_mute_delete(self):
         self.assertTrue(self.unitI.mute_delete(config['user']['target']))
 
-        print("\t\t=> blocking_create")
+    def test_0_blocking_create(self):
         self.assertEqual(type(self.unitI.blocking_create(config['user']['target'])), dict)
 
-        print("\t\t=> blocking_list")
+    def test_0_blocking_list(self):
         self.assertEqual(type(self.unitI.blocking_list()), list)
 
-        print("\t\t=> blocking_delete")
+    def test_0_blocking_delete(self):
         self.assertEqual(type(self.unitI.blocking_delete(config['user']['target'])), dict)
 
-        print("DONE\t=> users\n")
-
     def test_drive(self):
-        print("UNIT\t=> drive")
-        
-        print("\t\t=> drive")
         self.assertEqual(type(self.unitI.drive()), dict)
         
-        print("\t\t=> drive_files")
+    def test_drive_files(self):
         self.assertEqual(type(self.unitI.drive_files()), list)
         
-        print("\t\t=> drive_files_create")
-        uploadRes = self.unitI.drive_files_create(os.path.dirname(os.path.abspath(__file__)) + "/uploadTarget.png")
-        self.assertEqual(type(uploadRes), dict)
+    def test_0_drive_files_create(self):
+        self.__class__.driveFiles = self.unitI.drive_files_create(os.path.dirname(os.path.abspath(__file__)) + "/uploadTarget.png")
+        self.assertEqual(type(self.__class__.driveFiles), dict)
+    
+    def test_0_drive_files_uploadFromUrl(self):
+        self.__class__.driveFilesUrl = self.unitI.drive_files_uploadFromUrl(config['drive']['targetUrl'])
+        self.assertEqual(type(self.__class__.driveFilesUrl), dict)
 
-        print("\t\t=> drive_files_uploadFromUrl")
-        uploadRes_url = self.unitI.drive_files_uploadFromUrl(config['drive']['targetUrl'])
-        self.assertEqual(type(uploadRes_url), dict)
+    def test_1_drive_files_attachedNotes(self):
+        self.assertEqual(type(self.unitI.drive_files_attachedNotes(self.__class__.driveFiles['id'])), list)
 
-        print("\t\t=> drive_files_attachedNotes")
-        self.assertEqual(type(self.unitI.drive_files_attachedNotes(uploadRes['id'])), list)
-
-        print("\t\t=> drive_files_checkExistence")
-        self.assertTrue(self.unitI.drive_files_checkExistence(uploadRes['md5']))
+    def test_1_drive_files_checkExistence(self):
+        self.assertTrue(self.unitI.drive_files_checkExistence(self.__class__.driveFiles['md5']))
+        self.assertTrue(self.unitI.drive_files_checkExistence(self.__class__.driveFilesUrl['md5']))
         self.assertFalse(self.unitI.drive_files_checkExistence("hoge"))
 
-        print("\t\t=> drive_files_find")
+    def test_1_drive_files_find(self):
         self.assertEqual(type(self.unitI.drive_files_find("uploadTarget.png")), list)
 
-        print("\t\t=> drive_files_show")
-        self.assertEqual(type(self.unitI.drive_files_show(fileId=uploadRes['id'])), dict)
+    def test_1_drive_files_show(self):
+        self.assertEqual(type(self.unitI.drive_files_show(fileId=self.__class__.driveFiles['id'])), dict)
 
-        print("\t\t=> drive_files_update")
-        self.assertEqual(type(self.unitI.drive_files_update(uploadRes['id'], name="updatedTarget.png")), dict)
+    def test_2_drive_files_update(self):
+        self.assertEqual(type(self.unitI.drive_files_update(self.__class__.driveFiles['id'], name="updatedTarget.png")), dict)
 
-        print("\t\t=> drive_files_delete")
-        self.assertTrue(self.unitI.drive_files_delete(uploadRes['id']))
-        self.assertTrue(self.unitI.drive_files_delete(uploadRes_url['id']))
+    def test_9_drive_files_delete(self):
+        self.assertTrue(self.unitI.drive_files_delete(self.__class__.driveFiles['id']))
+        self.assertTrue(self.unitI.drive_files_delete(self.__class__.driveFilesUrl['id']))
         
-        print("\t\t=> drive_folders")
+    def test_drive_folders(self):
         self.assertEqual(type(self.unitI.drive_folders()), list)
 
-        print("\t\t=> drive_folders_create")
-        createFolder = self.unitI.drive_folders_create("testFolder")
-        self.assertEqual(type(createFolder), dict)
+    def test_0_drive_folders_create(self):
+        self.__class__.driveFolders = self.unitI.drive_folders_create("testFolder")
+        self.assertEqual(type(self.__class__.driveFolders), dict)
         
-        print("\t\t=> drive_folders_find")
+    def test_1_drive_folders_find(self):
         self.assertEqual(type(self.unitI.drive_folders_find("testFolder")), list)
 
-        print("\t\t=> drive_folders_show")
-        self.assertEqual(type(self.unitI.drive_folders_show(createFolder['id'])), dict)
+    def test_1_drive_folders_show(self):
+        self.assertEqual(type(self.unitI.drive_folders_show(self.__class__.driveFolders['id'])), dict)
 
-        print("\t\t=> drive_folders_update")
-        self.assertEqual(type(self.unitI.drive_folders_update(createFolder['id'], name="updatedTestFolder")), dict)
+    def test_2_drive_folders_update(self):
+        self.assertEqual(type(self.unitI.drive_folders_update(self.__class__.driveFolders['id'], name="updatedTestFolder")), dict)
 
-        print("\t\t=> drive_folders_delete")
-        self.assertTrue(type(self.unitI.drive_folders_delete(createFolder['id'])))
+    def test_9_drive_folders_delete(self):
+            self.assertTrue(type(self.unitI.drive_folders_delete(self.__class__.driveFolders['id'])))
 
-        print("\t\t=> drive_stream")
+    def test_drive_stream(self):
         self.assertEqual(type(self.unitI.drive_stream()), list)
 
-        print("DONE\t=> drive\n")
-
-    def test_messaging(self):
-        print("UNIT\t=> messaging")
-        
-        print("\t\t=> messaging_history")
+    def test_messaging_history(self):
         self.assertEqual(type(self.unitI.messaging_history()), list)
 
-        print("\t\t=> messaging_messages")
+    def test_messaging_messages(self):
         self.assertEqual(type(self.unitI.messaging_messages(config['user']['target'])), list)
 
-        print("\t\t=> messaging_messages_create")
+    def test_0_messaging_messages_create(self):
         mmc = self.unitI.messaging_messages_create(config['user']['target'], text="It works!")
         self.assertEqual(type(mmc), dict)
+        self.__class__.messagingId = mmc['id']
 
-        print("\t\t=> messaging_messages_read")
+    def test_messaging_messages_read(self):
         self.assertTrue(self.unitI.messaging_messages_read(config['messages']['target']))
 
-        print("\t\t=> messaging_messages_delete")
-        self.assertTrue(self.unitI.messaging_messages_delete(mmc['id']))
+    def test_9_messaging_messages_delete(self):
+        self.assertTrue(self.unitI.messaging_messages_delete(self.__class__.messagingId))
 
 def TESTSUITE():
     suite = unittest.TestSuite()
