0dda8cef8be517787af701070b443b1129902324
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-09-02 16:00:16 +0900

    2.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ ld = parse_from_file('README.md')
 
 setup(
     name='Misskey.py',
-    version='2.0.0',
+    version='2.1.0',
     description='The Misskey library for Python. Misskey is made by syuilo.',
 	long_description=ld,
 	test_suite='UNIT.TESTSUITE',
