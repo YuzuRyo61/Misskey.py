7205d42ccb34fc744e335f1040f815e70bd83f0c
YuzuRyo61 <yuzuryo61@yuzulia.com>
2020-05-28 21:27:11 +0900

    2.4.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 
 setup(
     name='Misskey.py',
-    version='2.4.0',
+    version='2.4.1',
     description='The Misskey library for Python. Misskey is made by syuilo.',
 	long_description=ld,
 	keywords="Misskey API syuilo",
