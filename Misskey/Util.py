92c7cda3bbb49ac732a0fc8d49676f917a19131a
YuzuRyo61 <yuzuryo61@yuzulia.com>
2020-05-30 10:50:15 +0900

    Delete MisskeyUtil class

--- Misskey/Util.py
+++ Misskey/Util.py
@@ -25,111 +25,105 @@ def deprecated(func): # pragma: no cover
     return new_func
 
 @deprecated
-class MisskeyUtil:
-    @staticmethod
-    @deprecated
-    def hash_apitoken(accessToken, appSecret): # pragma: no cover
-        """
-        **Deprecated**
-        The issued access token and app secret key are combined and hashed for use in API.
+def hash_apitoken(accessToken, appSecret): # pragma: no cover
+    """
+    **Deprecated**
+    The issued access token and app secret key are combined and hashed for use in API.
 
-        :param accessToken: Token Specify the access token issued at the time of authentication.
-        :param appSecret: Specify the app secret key.
-        :type accessToken: str
-        :type appSecret: str
-        :rtype: str
-        """
-        tokenraw = accessToken + appSecret
-        return hashlib.sha256(tokenraw.encode('utf-8')).hexdigest()
+    :param accessToken: Token Specify the access token issued at the time of authentication.
+    :param appSecret: Specify the app secret key.
+    :type accessToken: str
+    :type appSecret: str
+    :rtype: str
+    """
+    tokenraw = accessToken + appSecret
+    return hashlib.sha256(tokenraw.encode('utf-8')).hexdigest()
 
-    @staticmethod
-    @deprecated
-    def create_app(instanceAddress, appName, description, permission=[
-        'read:account',
-        'write:account',
-        'read:blocks',
-        'write:blocks',
-        'read:drive',
-        'write:drive',
-        'read:favorites',
-        'write:favorites',
-        'read:following',
-        'write:following',
-        'read:messaging',
-        'write:messaging',
-        'read:mutes',
-        'write:mutes',
-        'write:notes',
-        'read:notifications',
-        'write:notifications',
-        'read:reactions',
-        'write:reactions',
-        'write:votes'
-    ], callbackUrl=None): # pragma: no cover
-        """
-        **Deprecated**
-        Creates an application key with the specified instance address.
+@deprecated
+def create_app(instanceAddress, appName, description, permission=[
+    'read:account',
+    'write:account',
+    'read:blocks',
+    'write:blocks',
+    'read:drive',
+    'write:drive',
+    'read:favorites',
+    'write:favorites',
+    'read:following',
+    'write:following',
+    'read:messaging',
+    'write:messaging',
+    'read:mutes',
+    'write:mutes',
+    'write:notes',
+    'read:notifications',
+    'write:notifications',
+    'read:reactions',
+    'write:reactions',
+    'write:votes'
+], callbackUrl=None): # pragma: no cover
+    """
+    **Deprecated**
+    Creates an application key with the specified instance address.
 
-        :param instanceAddress: Specify the Misskey instance address.
-        :param appName: Specifies the name of the app.
-        :param description: Specify the app description.
-        :param permission: Specifies the app's permissions.
-        :param callbackUrl: Specify if there is a URL to call back after user authentication.
-        :type instanceAddress: str
-        :type appName: str
-        :type description: str
-        :type permission: list
-        :type callbackUrl: str or None
-        :rtype: dict
-        """
-        res = requests.post(f"https://{instanceAddress}/api/app/create", data=json.dumps({'name': appName, 'description': description, 'permission': permission, 'callbackUrl': callbackUrl}), headers={'content-type': 'application/json'})
+    :param instanceAddress: Specify the Misskey instance address.
+    :param appName: Specifies the name of the app.
+    :param description: Specify the app description.
+    :param permission: Specifies the app's permissions.
+    :param callbackUrl: Specify if there is a URL to call back after user authentication.
+    :type instanceAddress: str
+    :type appName: str
+    :type description: str
+    :type permission: list
+    :type callbackUrl: str or None
+    :rtype: dict
+    """
+    res = requests.post(f"https://{instanceAddress}/api/app/create", data=json.dumps({'name': appName, 'description': description, 'permission': permission, 'callbackUrl': callbackUrl}), headers={'content-type': 'application/json'})
 
-        if res.status_code != 200:
-            raise MisskeyAPIException('/app/create', 200, res.status_code, res.text)
-        else:
-            return json.loads(res.text)
+    if res.status_code != 200:
+        raise MisskeyAPIException('/app/create', 200, res.status_code, res.text)
+    else:
+        return json.loads(res.text)
 
-    @staticmethod
-    @deprecated
-    def session_generate(instanceAddress, appSecret): # pragma: no cover
-        """
-        **Deprecated**
-        Issue a token to authenticate the user.
+@deprecated
+def session_generate(instanceAddress, appSecret): # pragma: no cover
+    """
+    **Deprecated**
+    Issue a token to authenticate the user.
 
-        :param instanceAddress: Specify the Misskey instance address.
-        :param appSecret: Specifies the secret key.
-        :type instanceAddress: str
-        :type appSecret: str
-        :rtype: dict
-        """
-        res = requests.post(f"https://{instanceAddress}/api/auth/session/generate", data=json.dumps({'appSecret': appSecret}), headers={'content-type': 'application/json'})
+    :param instanceAddress: Specify the Misskey instance address.
+    :param appSecret: Specifies the secret key.
+    :type instanceAddress: str
+    :type appSecret: str
+    :rtype: dict
+    """
+    res = requests.post(f"https://{instanceAddress}/api/auth/session/generate", data=json.dumps({'appSecret': appSecret}), headers={'content-type': 'application/json'})
 
-        if res.status_code != 200:
-            raise MisskeyAPIException('/auth/session/generate', 200, res.status_code, res.text)
-        else:
-            return json.loads(res.text)
+    if res.status_code != 200:
+        raise MisskeyAPIException('/auth/session/generate', 200, res.status_code, res.text)
+    else:
+        return json.loads(res.text)
 
-    @staticmethod
-    @deprecated
-    def session_userkey(instanceAddress, appSecret, token): # pragma: no cover
-        """
-        **Deprecated**
-        It is a function to perform when the user authenticates with the browser.
+@deprecated
+def session_userkey(instanceAddress, appSecret, token): # pragma: no cover
+    """
+    **Deprecated**
+    It is a function to perform when the user authenticates with the browser.
 
-        :param instanceAddress: Specify the Misskey instance address.
-        :param appSecret: Specifies the secret key.
-        :param token: Specify the token issued before authentication.
-        :type instanceAddress: str
-        :type appSecret: str
-        :type token: str
-        :rtype: dict
-        """
-        res = requests.post(f"https://{instanceAddress}/api/auth/session/userkey", data=json.dumps({'appSecret': appSecret, 'token': token}), headers={'content-type': 'application/json'})
+    :param instanceAddress: Specify the Misskey instance address.
+    :param appSecret: Specifies the secret key.
+    :param token: Specify the token issued before authentication.
+    :type instanceAddress: str
+    :type appSecret: str
+    :type token: str
+    :rtype: dict
+    """
+    res = requests.post(f"https://{instanceAddress}/api/auth/session/userkey", data=json.dumps({'appSecret': appSecret, 'token': token}), headers={'content-type': 'application/json'})
 
-        if res.status_code != 200:
-            raise MisskeyAPIException('/auth/session/userkey', 200, res.status_code, res.text)
-        else:
-            return json.loads(res.text)
+    if res.status_code != 200:
+        raise MisskeyAPIException('/auth/session/userkey', 200, res.status_code, res.text)
+    else:
+        return json.loads(res.text)
 
 def username_available(instanceAddress, username):
     """
