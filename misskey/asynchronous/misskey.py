8ce34edf13ab51bb890bc66d2024fc3a98f1ec58
YuzuRyo61 <yuzuryo61@yuzulia.work>
2023-11-24 23:41:13 +0900

    Add docstring

--- misskey/asynchronous/misskey.py
+++ misskey/asynchronous/misskey.py
@@ -8,4 +8,10 @@ __all__ = (
 class AsyncMisskey(
     MeAsyncMisskey,
 ):
+    """
+    This class allows asynchronous processing and manipulation
+    of Misskey's API using aiohttp.
+    If the "async" extra is not given when installing the
+    Misskey.py library, it may not work properly.
+    """
     pass
